68557e72a6175f8f26666f3de5a002d931e12883
exersalza (Julian J.) <exersalza.dev@gmail.com>
2025-10-08 13:12:44 +0200

    update

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -5,6 +5,7 @@ const HIGHLIGHT_SIZE = (window.screen.height + window.screen.width) / 6
 
 export function App() {
     const screen = window.screen;
+    const maskId = "lines-circle-mask";
 
     const [[mX, mY], setMouse] = useState([0, 0]);
     const [mousePresent, setMousePresent] = useState(0);
@@ -26,39 +27,61 @@ export function App() {
     }
 
 
-    const renderSvg = () => {
-        const svg = stonksLineRef.current;
 
-        if (!svg) return;
-
-        Array.from(svg.querySelectorAll("line")).forEach((el) => el.remove());
+    const calcPoints = (svg: SVGSVGElement) => {
+        let points = [];
 
         const width = svg.clientWidth;
         const height = svg.clientHeight;
         const numberOfLines = 50;
 
+        let last_x2 = 0;
+        let last_y2 = Math.random() * height;
+
+
+        for (let i = 0; i < numberOfLines; i++) {}
+
+    }
+
+
+    const renderSvg = () => {
+        const svg = stonksLineRef.current;
+
+        if (!svg) return;
+
+        // Array.from(svg.querySelectorAll("line")).forEach((el) => el.remove());
+
+        const width = svg.clientWidth;
+        const height = svg.clientHeight;
+        const numberOfLines = 50;
+
+        let last_x2 = 0;
+        let last_y2 = Math.random() * height;
+
         for (let i = 0; i < numberOfLines; i++) {
-            const x1 = Math.random() * width;
-            const y1 = Math.random() * height;
-            const x2 = Math.random() * width;
-            const y2 = Math.random() * height;
+            const x2 = last_x2 + width / numberOfLines;
+            const y2 = Math.random() * height ;
 
             const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-            line.setAttribute("x1", x1);
-            line.setAttribute("y1", y1);
+            line.setAttribute("x1", last_x2);
+            line.setAttribute("y1", last_y2);
             line.setAttribute("x2", x2);
             line.setAttribute("y2", y2);
             line.setAttribute("stroke", "green");
-            line.setAttribute("mask", "url(#test)");
+            // line.setAttribute("mask", `url(#${maskId})`);
             line.setAttribute("stroke-width", "2");
 
             svg.appendChild(line);
+
+            last_x2 = x2;
+            last_y2 = y2;
         }
     }
 
 
     useEffect(() => {
         renderSvg()
+
         let refsda = screenRef.current;
 
         if (refsda) {
@@ -94,15 +117,23 @@ export function App() {
                 }
             </div>
 
-
-            <svg
-                ref={stonksLineRef}
-                className={"h-screen w-screen absolute top-0 left-0 z-15 text-clip"}
-            >
-                <mask x={mY} y={mX} id="test">
-                    <rect width="100%" height="100%" fill="white" />
-                    <rect x="100" y="50" width="100" height="100" fill="black" />
-                </mask>
+            <svg ref={stonksLineRef} className="h-screen w-screen absolute top-0 left-0 z-15 text-clip">
+                <defs>
+                    <radialGradient id="softMaskGradient" cx="50%" cy="50%" r="50%">
+                        <stop offset="0" stop-color="white" stop-opacity="1" />
+                        <stop offset="1" stop-color="white" stop-opacity=".01" />
+                    </radialGradient>
+                    <mask id={maskId}>
+                        <rect x="0" y="0" width="100%" height="100%" fill="black" />
+                        <circle
+                            cx={mY + HIGHLIGHT_SIZE / 2}
+                            cy={mX + HIGHLIGHT_SIZE / 2}
+                            r={HIGHLIGHT_SIZE / 2}
+                            fill="url(#softMaskGradient)"
+                            style={{ opacity: mousePresent }}
+                        />
+                    </mask>
+                </defs>
             </svg>
 
 
